670fe976cc4d24ab3bef98232309af92bea2f29d
Jayabalaji <sathiyamoorthi.jb@gmail.com>
2025-04-09 12:36:42 +0200

    Update radio_script.sh
    keep the script generic and fix some shellcheck issues

--- radio_script.sh
+++ radio_script.sh
@@ -3,14 +3,11 @@
 # Author : JB
 
 # Read Bluetooth MAC addresses from environment variables
-JABRA="$BT_MAC1"
-BOSE="$BT_MAC2"
+SPEAKER1="$BT_MAC1"
+SPEAKER2="$BT_MAC2"
 
 VLC_HOST=localhost
 VLC_RC_PORT=9999
-
-MAX_ATTEMPTS=25
-DELAY=10
 
 # Check if .env variables are set (error out if not)
 if [ -z "$BT_MAC1" ] || [ -z "$BT_MAC2" ]; then
@@ -36,60 +33,68 @@ check_and_unblock_bluetooth() {
 
 # Function to connect to Bluetooth speaker
 connect_bt() {
-    local BT_MAC=$1
+    local bt_mac=$1
     # Check if already connected
-    if bluetoothctl info $BT_MAC | grep -q "Connected: yes"; then
-        echo "$BT_MAC is already connected."
+    if bluetoothctl info "$bt_mac" | grep -q "Connected: yes"; then
+        echo "$bt_mac is already connected."
         return 0
     fi
 
-    echo "Attempting to connect to Bluetooth speaker at $BT_MAC..."
+    echo "Attempting to connect to Bluetooth speaker at $bt_mac..."
     bluetoothctl << EOF
 power on
 agent on
 default-agent
-connect $BT_MAC
+connect $bt_mac
 EOF
 }
 
 # Function to play the radio
 play_radio() {
+    local bt_mac=$1
     # Set the Bluetooth speaker as the default audio sink
-    SINK_NAME=$(echo $1 | sed 's/:/_/g')
-    pactl set-default-sink bluez_sink.$SINK_NAME.a2dp_sink
+    local sink_name
+    sink_name=${bt_mac//:/_}
+    pactl set-default-sink "bluez_sink.$sink_name.a2dp_sink"
 
     if ! pgrep -x vlc > /dev/null; then
         echo "Starting VLC to play the stream..."
-        vlc --intf rc --rc-host ${VLC_HOST}:${VLC_RC_PORT} /app/playlist.m3u &
+        vlc --intf rc --rc-host "${VLC_HOST}:${VLC_RC_PORT}" /app/playlist.m3u &
         sleep 5
     else
         echo "VLC is already running."
     fi
 }
 
+# Function to stop the radio
+stop_radio() {
+    pkill -f vlc
+    echo "Stopped radio."
+}
+
 # Main script execution starts here
 
 # Check if script is passed 'play', 'next', or 'stop'
-INPUT=$1
-if [ "$INPUT" != "play" ] && [ "$INPUT" != "next" ] && [ "$INPUT" != "stop" ]; then
+input=$1
+if [ "$input" != "play" ] && [ "$input" != "next" ] && [ "$input" != "stop" ]; then
     echo "Invalid input. Usage: $0 {play|next|stop}"
     exit 1
 fi
 
-case $INPUT in
+case $input in
     play)
         check_and_unblock_bluetooth
-        connect_bt "$JABRA" || connect_bt "$BOSE"
-        play_radio "$JABRA" || play_radio "$BOSE"
+        connect_bt "$SPEAKER1" || connect_bt "$SPEAKER2"
+        play_radio "$SPEAKER1" || play_radio "$SPEAKER2"
         ;;
     next)
         # Logic for next track
         ;;
     stop)
-        pkill -f vlc
-        echo "Stopped radio."
+        stop_radio
         ;;
     *)
         echo "Invalid command."
+        exit 1
         ;;
 esac
